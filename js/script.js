0cde5eb0d91f360bc47a1ee4412f8e1687212532
go.ugotsta.com <ugotsta@gmail.com>
2017-07-28 13:11:56 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -140,7 +140,7 @@ jQuery(document).ready(function() {
     }
     
     function toc_html() {
-        var html = '<h3>Table of Contents</h3>';
+        var html = '';
         // iterate section classes and get id name to compose TOC
         $( '#commands .section' ).each(function() {
             var name = $( this ).attr( 'id' );
@@ -157,6 +157,7 @@ jQuery(document).ready(function() {
         var html = '<a href="https://github.com/Ugotsta/alexa-cheats"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>';
         
         // render TOC
+        html += '<h3>Table of Contents</h3>';
         html += '<div id="toc">';
         html += toc_html();
         html += '</div>';
