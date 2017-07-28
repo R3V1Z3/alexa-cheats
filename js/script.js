21332d7d33f8856e555e80607e293b39007e4541
go.ugotsta.com <ugotsta@gmail.com>
2017-07-28 04:00:48 -0700

    Fixed link to markdown file

--- js/script.js
+++ js/script.js
@@ -123,7 +123,7 @@ jQuery(document).ready(function() {
     function render_info() {
         
         // GitHub Ribbon
-        var html = '<a href="https://github.com/Ugotsta/alexa-commands"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>';
+        var html = '<a href="https://github.com/Ugotsta/alexa-cheats"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>';
         
         // render TOC
         html += '<div id="toc">';
@@ -150,7 +150,7 @@ jQuery(document).ready(function() {
     }
 
     $.ajax({
-        url: 'alexa-commands.md',
+        url: 'alexa-cheats.md',
         type: 'GET',
         dataType: 'text'
     }).success(function(content) {
